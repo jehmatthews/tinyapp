9bb6cf70415dd446ebdddb328c179c375e100464
jehmatthews <jehmatthews30@gmail.com>
2022-09-24 13:56:32 -0700

    comment for more clairty of what how functions operate

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 const { findUser, generateRandomString } = require('./helpers');
 
 app.use(cookieSession({ 
-  name: 'session', // where cookies are encrypted
+  name: 'session', 
   keys: ['key1']
 }));
 
@@ -52,6 +52,7 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// Redirects to /urls if logge in, if not logged in, directed to /login
 app.get('/', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
@@ -62,15 +63,15 @@ app.get('/', (req, res) => {
 
   if (!user) {
     res.redirect('/login');
-  }
+  };
 });
 
-// Get request for URL's
+// Get request for URL's, shows URL's belonging to user logged in
 app.get('/urls', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
 
-  templateVars = { 
+  const templateVars = { 
     urls: urlsForUser(cookieID),
     user: user
   };
@@ -82,11 +83,11 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// Get request for new URL's
+// Get request for new URL's, checks to make sure user is logged in before displaying
 app.get('/urls/new', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
-  templateVars = {
+  const templateVars = {
     user: user
   };
 
@@ -96,7 +97,7 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new', templateVars);
 });
 
-// Get request for the edit page
+// Get request for the edit page, if id found, can alter
 app.get('/urls/:id', (req, res) => {
   const { id } = req.params;
   const cookieID = req.session.user_id;
@@ -118,17 +119,18 @@ app.get('/u/:id', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
   const longURL = urlDatabase[req.params.id].longUrl;
+
   if (!user) {
     res.send("<html><body>No login</body></html>");
   };
   if (!urlDatabase[req.params.id]) {
-    res.send("<html><body>Cannot find ID</body></html>")
+    res.send("<html><body>Cannot find ID</body></html>");
   } else {
-    res.redirect(longURL)
+    res.redirect(longURL);
   };
 });
 
-// Get request for registering new user
+// Get request for registering new user, if registered, directs to urls
 app.get('/register', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
@@ -143,7 +145,7 @@ app.get('/register', (req, res) => {
   res.render('urls_register', templateVars);
 });
 
-// Get request for login from valid user
+// Get request for login from valid user, if valid, logs in and redirects to /urls
 app.get('/login', (req, res) => {
   const cookieID = req.session.user_id;
   const user = users[cookieID];
@@ -154,14 +156,14 @@ app.get('/login', (req, res) => {
   };
 
   if (user) {
-    res.redirect('/urls')
+    res.redirect('/urls');
     return;
   };
   res.render('urls_login', templateVars);
 });
 
 // ----- Post requests ----- 
-// Post when registering new user
+// Post when registering new user, if valid creds, redirect to /urls
 app.post('/register', (req, res) => {
   const key = generateRandomString();
   const hashPassword = bcrypt.hashSync(req.body.password, 10);
@@ -185,7 +187,7 @@ app.post('/register', (req, res) => {
   return;
 });
 
-// Post when logging in user
+// Post when logging in user, if valid creds, redirect to /urls
 app.post('/login', (req, res) => {
   const user = findUser(req.body.email, 'email', users);
 
@@ -201,7 +203,7 @@ app.post('/login', (req, res) => {
   return;
 });
 
-// Post when adding new URL's
+// Post when adding new URL's, adds new url to database
 app.post('/urls', (req, res) => {
   const key = generateRandomString();
   const cookieID = req.session.user_id;
@@ -210,6 +212,7 @@ app.post('/urls', (req, res) => {
   if (!user) {
     return res.send("<html><body>Permission denied</body></html>");
   }; 
+
   urlDatabase[key] = {
     id: key,
     longUrl: req.body.longURL,
