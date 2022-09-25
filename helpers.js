dc0ba627da7bbdb7db129111eef8f702b8e14b54
jehmatthews <jehmatthews30@gmail.com>
2022-09-25 14:28:13 -0700

    'users', 'urlDatabase' and 'urlsForUsers' added to helpers file and are exported to server

--- helpers.js
+++ helpers.js
@@ -1,3 +1,23 @@
+// for URLs created
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+// for Users created
+const users = {  
+  userRandomID: {
+  id: "userRandomID",
+  email: "user@example.com",
+  password: "purple-monkey-dinosaur",
+},
+user2RandomID: {
+  id: "user2RandomID",
+  email: "user2@example.com",
+  password: "dishwasher-funk",
+},
+};
+
 const findUser = (email, key, users) => { // finds user by their email
   for (const user in users) {
     let userEmail = users[user];
@@ -18,4 +38,14 @@ function generateRandomString() { // function that gives a 6 char id to cookie a
   return result;
 };
 
-module.exports = { findUser, generateRandomString };
+const urlsForUser = (id) => { // returns object based on matching of url and id
+  const urls = {};
+  for (const url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      urls[url] = urlDatabase[url];
+    }
+  }
+  return urls;
+};
+
+module.exports = { urlDatabase, users, findUser, generateRandomString, urlsForUser };
